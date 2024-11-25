5112a313e820925bd9e62e5a5c1dcbe49bc1c7a5
Reya Sadhu <65485231+reyasadhu@users.noreply.github.com>
2024-11-25 14:08:20 -0800

    Update two_tower_model.py

--- two_tower_model.py
+++ two_tower_model.py
@@ -8,29 +8,29 @@ import tensorflow_recommenders as tfrs
 class TwoTowerModel(tfrs.models.Model):
   """ Retrieval model encompassing users as query and items as candidates"""
 
-  def __init__(self, user_model, item_model, product_features):
+  def __init__(self, user_tower, item_tower, product_features):
     super().__init__()
-    self.user_model = user_model
-    self.item_model = item_model
+    self.user_tower = user_tower
+    self.item_tower = item_tower
     self.product_features = product_features
 
-    # Query tower
-    self.query_model = models.Sequential()
-    self.query_model.add(self.user_model)
-    self.query_model.add(layers.Dense(64, activation="relu"))
-    self.query_model.add(layers.Dense(32, activation="relu"))
-    self.query_model.add(layers.Dense(32))
+    # User tower
+    self.user_model = models.Sequential()
+    self.user_model.add(self.user_tower)
+    self.user_model.add(layers.Dense(64, activation="relu"))
+    self.user_model.add(layers.Dense(32, activation="relu"))
+    self.user_model.add(layers.Dense(32))
 
-    # Candidate tower
-    self.candidate_model = models.Sequential()
-    self.candidate_model.add(self.item_model)
-    self.candidate_model.add(layers.Dense(64, activation="relu"))
-    self.candidate_model.add(layers.Dense(32, activation="relu"))
-    self.candidate_model.add(layers.Dense(32))
+    # Item tower
+    self.item_model = models.Sequential()
+    self.item_model.add(self.item_tower)
+    self.item_model.add(layers.Dense(64, activation="relu"))
+    self.item_model.add(layers.Dense(32, activation="relu"))
+    self.item_model.add(layers.Dense(32))
 
     # Retrieval task for loss function
     metrics = tfrs.metrics.FactorizedTopK(
-            candidates=product_features.batch(128).map(self.item_model)
+            candidates=product_features.batch(128).map(self.item_tower)
     )
     self.task = tfrs.tasks.Retrieval(metrics=metrics)
   
